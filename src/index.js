951455e3fb454eab8f09cb8927b4b46ed3d9e52c
Aleja22LC <alejaleona@hotmail.com>
2022-07-22 20:06:42 +0200

    subiendo inicio de funcionalidad

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import validator from './validator.js';
 
 console.log(validator);
+const btnpay = document.getElementById("btnpay")
+btnpay.addEventListener("click", numerocard)
+
+/*obtener el valor que esta en numerocard*/
+function numerocard() {
+    let inputvalue = document.getElementById("numerocard").value;
+    console.log(inputvalue)
+    //alert("funciona")
+    /*invoque la funcion is.valid */
+    console.log(validator.isValid(inputvalue))   
+
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,18 @@
 const validator = {
-  // ...
+    isValid: function (creditCardNumber) {
+
+    //convertir un string en un array
+        const digits= Array.from(creditCardNumber) 
+    //recorrer cada digito
+    //en los digitos pares iniciando de 0
+    //se multiplican por dos 
+    //si el resultado es igual o mayores de 10 sumar los dos digitos
+      
+        
+    }
+
+
 };
+
 
 export default validator;
