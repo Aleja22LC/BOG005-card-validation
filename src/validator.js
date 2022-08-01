63973667d3c7117b092bb3bdb2e4b6d9f67d2237
Aleja22LC <alejaleona@hotmail.com>
2022-08-01 18:20:39 +0200

    funcion isvalid completa

--- src/validator.js
+++ src/validator.js
@@ -11,16 +11,19 @@ const validator = {
         console.log(reversedigits)
 
         //declararar variable para la suma de los valores finales pares e impares
-        const sumatotal = [];
+        let sumatotal = [];
 
+        let todoslosarray = []
         //recorrer cada digito del array (posicion "posi" y valor "val")
         reversedigits.forEach((val, posi) => {
             //posi = aisla las posiciones pares
+            
             if (posi % 2 === 0) {
                 //se multiplican por 2 los valores aislados
                 let pares = val * 2
-                console.log(pares)
+                todoslosarray.push(pares)
 
+                // console.log(arraypares)
                 //sumo los numeros mayores de 9 o de con digitos
                 if (pares > 9) {
                     //los #>9=pares al sumar sus digitos y restar este resultado obtenido siempre da 9
@@ -29,20 +32,24 @@ const validator = {
                     //parseint me devuelve el numero entero despues de separarlo en digitos
                     sumatotal.push(pares.toString().split("").reduce((a, b) => parseInt(a) + parseInt(b)));
 
-                    console.log("cambioparametros")
+                    //console.log("cambioparametros")
 
                 } else {
                     sumatotal.push(pares);
                 }
 
             } else {
-                sumatotal.push(parseInt(val));
-                console.log("elseparaimpares")
+                todoslosarray.push(parseInt(val))
+
+                // sumatotal.push(parseInt(val));
+                // console.log("elseparaimpares")
             }
 
         });
+        console.log(todoslosarray)
         //como sumo los numeros que hay dentro de un array
-        const sumaarray = sumatotal.reduce((a, b) => a + b);
+        const sumaarray = todoslosarray.reduce((a, b) => a + b);
+        console.log(sumaarray)
 
         //como verificar que un numero de un array es multiplo de 10
         if (sumaarray % 10 === 0) {
@@ -52,10 +59,16 @@ const validator = {
         }
     }
 
+    // maskify: function (creditCardNumber) {
+    //     var simbolo = '#';
+    //     var mascara = (creditCardNumber.slice(0, -4).replace(/./g, simbolo) + ('' + creditCardNumber.slice(-4)));
+    //     return mascara;
+    // }
+
   
 
 
-
+// cardnumber valid 491268510318
 
 };
 
