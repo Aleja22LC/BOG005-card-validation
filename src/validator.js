e71159802417a8443f5e75906e50f6ff65558934
Aleja22LC <alejaleona@hotmail.com>
2022-08-01 16:50:02 +0200

    subiendo isvalid_0

--- src/validator.js
+++ src/validator.js
@@ -1,50 +1,61 @@
 const validator = {
     isValid: function (creditCardNumber) {
+        //invoca cualquier funcionalidad de depuración disponible
+        //debugger;
 
         //convertir un string en un array
         const digits = Array.from(creditCardNumber)
-        //https://es.stackoverflow.com/questions/154491/suma-de-cifras-de-un-numero-en-javascript
-        let salida = [];
 
-        //obtener la reversa del array (metodo reverse(), altera el array que se ejecuta)
-        digits.reverse();
-        console.log(digits)
+        //creo variable para la reversa //obtener la reversa del array        
+        let reversedigits = digits.reverse();
+        console.log(reversedigits)
+
+        //declararar variable para la suma de los valores finales pares e impares
+        const sumatotal = [];
 
         //recorrer cada digito del array (posicion "posi" y valor "val")
-        digits.forEach((val, posi) => {
+        reversedigits.forEach((val, posi) => {
             //posi = aisla las posiciones pares
             if (posi % 2 === 0) {
                 //se multiplican por 2 los valores aislados
                 let pares = val * 2
+                console.log(pares)
 
-                // si el resultado es mayor de 9 se suman los digitos de dos cifras
+                //sumo los numeros mayores de 9 o de con digitos
                 if (pares > 9) {
-                    //push añade un valor a un array, luego lo convierto en cadena, lo divido o separo, 
-                    //.reduce"reduce cada elemento y lo devuelve un unico valor"
-                    salida.push(pares.toString().split("").reduce((a, b) =>
-                        //parseint vuelve numeros los valores separados
-                        parseInt(a) + parseInt(b)));
+                    //los #>9=pares al sumar sus digitos y restar este resultado obtenido siempre da 9
+                    let sumapares = pares - 9
+                    // uso push para modificar o cambiar los elemnetos del array 
+                    //parseint me devuelve el numero entero despues de separarlo en digitos
+                    sumatotal.push(pares.toString().split("").reduce((a, b) => parseInt(a) + parseInt(b)));
+
+                    console.log("cambioparametros")
+
+                } else {
+                    sumatotal.push(pares);
                 }
-                //consigo mis valores pares
-                else {
-                    salida.push(pares);
-                }
-                
-                //consigo los valores impares
-                else {
-                salida.push(parseInt(val));
+
+            } else {
+                sumatotal.push(parseInt(val));
+                console.log("elseparaimpares")
             }
-            
-        }});
-    
-        
-    
-      
-        
+
+        });
+        //como sumo los numeros que hay dentro de un array
+        const sumaarray = sumatotal.reduce((a, b) => a + b);
+
+        //como verificar que un numero de un array es multiplo de 10
+        if (sumaarray % 10 === 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    //como sumo los numeros que hay dentro de un array
-    //como verificar que un numero de un array es multiplo de 10
+  
+
+
+
 
 };
 
