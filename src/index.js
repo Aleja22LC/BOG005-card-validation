95df6a2acfbbef45f4cb856fc6dee897981a56bb
Aleja22LC <alejaleona@hotmail.com>
2022-08-02 23:56:00 +0200

    alertas creadas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import validator from './validator.js';
 
 console.log(validator);
-const btnpay = document.getElementById("btnpay")
+const btnpay = document.getElementById("btnpay");
 btnpay.addEventListener("click", numerocard)
 
 /*obtener el valor que esta en numerocard*/
@@ -11,10 +11,51 @@ function numerocard() {
     //alert("funciona")
     /*invoque la funcion is.valid */
     console.log(validator.isValid(inputvalue))
-    //llamo la funcion de masky 
+    //llamo la funcion de maskify 
     console.log(validator.maskify(inputvalue))
 
 }
 
-//
-//btn.addEventListener ("click", )
+document.getElementById("btn1").addEventListener("click", () => {
+    document.getElementById("tablabasica").style.display = "block";
+    document.getElementById("infopago").style.display = "none";
+})
+//boton pagar que ejecuta la validacion de la tc y el enmascarado
+document.getElementById("btnpay").addEventListener("click", () => {
+    let name = document.getElementById("nombre").value;
+    let expe = document.getElementById("fechaexp").value;
+    let codi = document.getElementById("ccv").value;
+    let entrada = document.getElementById("numerocard").value;
+    //llamo la funcion isvalid
+    let aprobado = validator.isValid(entrada);
+    
+    
+    if (name === '') {
+        return alert("POR FAVOR INTRODUZCA NOMBRE DEL TITULAR")
+    }
+    if (expe === '') {
+        return alert("POR FAVOR INGRESE FECHA DE EXPEDICION DE LA TARJETA")
+    }
+    if (codi === '') {
+        return alert("POR FAVOR INGRESE CÓDIGO DE VERIFICACION DE LA TARJETA")
+    }
+    if (entrada === '') {
+        return alert("POR FAVOR INGRESE EL NUMERO DE  LA TARJETA")
+    }
+    console.log(aprobado);
+
+    //enmascaro el numero dentro del input
+    let enmascarar = validator.maskify(entrada);
+
+    document.getElementById("numerocard").value = enmascarar;
+
+    console.log(enmascarar)
+
+     if (aprobado == true) {
+         return alert("¡NUMERO DE TARJETA VALIDO!")
+     } else {
+         aprobado == false
+         return alert("¡NUMERO DE TARJETA NO VALIDO, POR FAVOR INTENTE NUEVAMENTE!")
+     }
+})
+
